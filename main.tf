c368ed1dcc5d1b6f83063b752ac07aa0a8b6a17e
Brian F <bfaver@gmail.com>
2020-01-21 19:48:12 +0000

    Updated my code 2.

--- main.tf
+++ main.tf
@@ -112,8 +112,10 @@ resource aws_instance "hashicat" {
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
-  tags = {
+  tags = { 
     Name = "${var.prefix}-hashicat-instance"
+    Billable = "true"
+    Department = "devops"
   }
 }
 
